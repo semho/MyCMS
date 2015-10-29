aa52a5c12bf62f7bd879cbe4623588c3584fe8d4
SEMEN <semho@yandex.ru>
2015-10-29 17:20:31 +0300

    added file config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = 'localhost';                  //имя сервера БД
+$dbname = 'myCMS';                      //имя БД
+$dbuser = 'mysql';                      //имя пользователя БД
+$dbpasswd = 'mysql';                    //пароль пользователя
+
+//информация о сайте
+$title = 'CMS v0.01';
+$description = 'Описание сайта';
+$info = '<p>&copy Семен Хохалев</p>';
